16595213bdbdde3f7dbc88b2c3ccc3ccd5b65995
Ahmad <Ahmadelfalogy@gmail.com>
2019-06-21 15:53:05 +0200

    Add ending footer bar and style the main footer , resize it and rewrite the caption of about website >>>>
    Edit orderbill and notification icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,73 +1,113 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
-const Footer = (props) => {
+import React from "react";
+import { NavLink } from "react-router-dom";
+const Footer = props => {
   return (
-    <section className="container-color">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <footer className="ftco-footer">
-                <div className="container">
-                  <div className="row mb-5">
-                    <div className="col-md">
-                      <div className="ftco-footer ftco-footer-widget mb-4">
-                        <h2 className="ftco-heading-2">About Website </h2>
-                        <p> Site for ordering your food from your favorite restaurant individually or in group . </p>
-                        <ul className="ftco-footer ftco-footer-social list-unstyled float-md-left float-left mt-5">
-                          <li className="ftco-animate"><NavLink className="a" to="/"><span className="fa fa-twitter"></span></NavLink></li>
-                          <li className="ftco-animate"><NavLink className="a" to="/"><span className="fa fa-facebook"></span></NavLink></li>
-                          <li className="ftco-animate"><NavLink className="a" to="/"><span className="fa fa-google"></span></NavLink></li>
-                        </ul>
-
-                      </div>
-                    </div>
-                    <div className="col-md">
-                      <div className="ftco-footer ftco-footer-widget mb-4 ml-md-4 ">
-                        <h2 className="ftco-heading-2">Links</h2>
-                        <ul className="list-unstyled">
-                          <li><NavLink to="/"  className="a"><span className="fa fa-long-arrow-right"></span> Home</NavLink></li>
-                          <li><NavLink to="/restaurants"  className="a"><span className="fa fa-long-arrow-right"></span> All Restaurant</NavLink></li>
-                          <li><NavLink to="/contact"  className="a"><span className="fa fa-long-arrow-right"> </span> Contact Us</NavLink></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="col-md">
-                      <div className="ftco-footer ftco-footer-widget mb-4">
-                        <h2 className="f-heading-2">Services</h2>
-                        <ul className="list_unstyled">
-                          <li><NavLink to="/profile/grouporder" className="a"><span className="fa fa-long-arrow-right mr-2"></span> Group Ordering</NavLink></li>
-                        </ul>
-                      </div>
-                    </div>
-
-                    <div className="col-md">
-                      <div className="ftco-footer ftco-footer-widget mb-4">
-                        <h2 className="f-heading-2">Contact US</h2>
-                        <div className="block-23 mb-3">
-                          <ul className="list_unstyled">
-                            <li><span className="fa fa-map-marker "> </span> <span className="text"> ITI Ismailia , Egypt</span></li>
-                            <li><span className="fa fa-phone "></span> <span className="text">  +20 1270440168</span></li>
-                            <li><span className="fa fa-envelope "></span> <span className="text"> info@hungry.com</span></li>
-
-                          </ul>
-                        </div>
-                      </div>
+    <section className="container-color pb-2">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <footer className="ftco-footer">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md">
+                    <div className="ftco-footer ftco-footer-widget mb-4">
+                      <h2 className="ftco-heading-2">About Website </h2>
+                      <p className="about-content">
+                        {" "}
+                        For ordering & discovering your favourite food from the
+                        restaurants alone or in a group .{" "}
+                      </p>
+                      <ul className="ftco-footer ftco-footer-social list-unstyled float-md-left float-left mt-3">
+                        <li className="ftco-animate">
+                          <NavLink className="a" to="/">
+                            <span className="fa fa-twitter" />
+                          </NavLink>
+                        </li>
+                        <li className="ftco-animate">
+                          <NavLink className="a" to="/">
+                            <span className="fa fa-facebook" />
+                          </NavLink>
+                        </li>
+                        <li className="ftco-animate">
+                          <NavLink className="a" to="/">
+                            <span className="fa fa-google" />
+                          </NavLink>
+                        </li>
+                      </ul>
                     </div>
                   </div>
-                  <div className="row">
-                    <div className="col-md-12 text-center">
-                      <p>Copyright &copy;
-                    <script type="document.write(newDate());"></script> All Rights Reserved</p>
+                  <div className="col-md">
+                    <div className="ftco-footer ftco-footer-widget mb-4 ml-md-4 ">
+                      <h2 className="ftco-heading-2 ml-3">Links</h2>
+                      <ul className="list-unstyled">
+                        <li>
+                          <NavLink to="/" className="a my-1">
+                            <span className="fa fa-home mr-3" /> Home
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink to="/restaurants" className="a my-3 d-block">
+                            <span className="fa fa-tags mr-3" /> All Restaurant
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-md">
+                    <div className="ftco-footer ftco-footer-widget mb-4">
+                      <h2 className="ftco-heading-2 ml-3">Services</h2>
+                      <ul className="list_unstyled">
+                        <li>
+                          <NavLink to="/profile/grouporder" className="a">
+                            <span className="fa fa-users mr-2" /> Group Ordering
+                          </NavLink>
+                        </li>
+                        <li>
+                          <NavLink to="/contact" className="a my-1">
+                            <span className="fa fa-phone mr-3"> </span> Contact
+                            Us
+                          </NavLink>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className="col-md">
+                    <div className="ftco-footer ftco-footer-widget mb-4">
+                      <h2 className="ftco-heading-2 ml-2">Contact US</h2>
+                      <div className="block-23 mb-3">
+                        <ul className="list_unstyled">
+                          <li>
+                            <span className="fa fa-map-marker mr-2 p-2 rounded-circle">
+                              {" "}
+                            </span>{" "}
+                            <span className="text"> Ismailia , Egypt</span>
+                          </li>
+                          <li className="my-3 d-block">
+                            <span className="fa fa-phone mr-2 p-2 rounded-circle" />{" "}
+                            <span className="text"> +20 12345678</span>
+                          </li>
+                          <li>
+                            <span className="fa fa-envelope  mr-2 p-2 rounded-circle" />{" "}
+                            <span className="text"> info@hungry.com</span>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </footer>
-            </div>
+              </div>
+            </footer>
           </div>
         </div>
-      </section>
+      </div>
+      <footer className="footer d-flex flex-direction-row justify-content-center align-items-center">
+        <h3 className="footer__content">
+          @ All copyright reserved 2019 | Hungry
+        </h3>
+      </footer>
+    </section>
   );
-
-}
+};
 
 export default Footer;
